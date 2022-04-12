b631d7c4ca33e867f3b58f251d71aca7e4f6e9c0
bijoya <bijoya10101@gmail.com>
2022-04-12 17:53:24 +0530

    flask api

--- demographic_filtering.py
+++ demographic_filtering.py
@@ -1,20 +1,8 @@
 import pandas as pd
-import numpy as np
 
+# create a dataframe using final.csv file
 df = pd.read_csv('final.csv')
 
-C = df['vote_average'].mean()
-m = df['vote_count'].quantile(0.9)
-q_movies = df.copy().loc[df['vote_count'] >= m]
-
-def weighted_rating(x, m=m, C=C):
-    v = x['vote_count']
-    R = x['vote_average']
-    return (v/(v+m) * R) + (m/(m+v) * C)
-
-q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
-
-q_movies = q_movies.sort_values('score', ascending=False)
-
-output = q_movies[['title', 'poster_link', 'release_date', 'runtime', 'vote_average', 'overview']].head(20).values.tolist()
-
+# sorting dataframe : wrt to weighted rating col in ascending order
+df = df.sort_values('weighted_rating' , ascending = False)
+output = df[['original_title' , 'poster_link' , 'runtime', 'release_date' , 'weighted_rating' ]].head(20)
--- main.py
+++ main.py
@@ -1,111 +1,129 @@
 from flask import Flask, jsonify, request
-
-from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
 from demographic_filtering import output
 from content_filtering import get_recommendations
+import pandas as pd
 
-_d = {
-        "title": all_movies.iloc[0,19],
-        "poster_link": all_movies.iloc[0,27],
-        "release_date": all_movies.iloc[0,13] or "N/A",
-        "duration": all_movies.iloc[0,15],
-        "rating": int(all_movies.iloc[0,20]/2)
-}
+movies_data = pd.read_csv('final.csv')
+all_movies = movies_data[["original_title","poster_link","release_date","runtime","weighted_rating"]]
+
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
+
+def assign_val():
+    m_data = {
+        "original_title": all_movies.iloc[0,0],
+        "poster_link": all_movies.iloc[0,1],
+        "release_date": all_movies.iloc[0,2] or "N/A",
+        "duration": all_movies.iloc[0,3],
+        "rating":all_movies.iloc[0,4]/2
+    }
+    return m_data
+        
 app = Flask(__name__)
 
 @app.route("/movies")
 def get_movie():
-    global _d,all_movies
+    movie_data=assign_val()
+
     return jsonify({
-        "data": _d,
+        "data": movie_data,
         "status": "success"
     })
 
 @app.route("/like", methods=["POST"])
 def liked_movie():
     global all_movies
-    global liked_movies
-    global _d
-    liked_movies.append(_d)
-    print(liked_movies)
-    print(all_movies)
-    all_movies=all_movies.iloc[1: , :]
-    all_movies=all_movies.reset_index()
-    print(all_movies)
+    movie_data=assign_val()
+    liked_movies.append(movie_data)
+    all_movies.drop([0], inplace=True)
+    all_movies=all_movies.reset_index(drop=True)
     return jsonify({
         "status": "success"
-    }), 201
+    })
+
+@app.route('/liked' , methods = ['GET'])
+def liked():
+    global liked_movies    
+
+    return jsonify({
+        'data' : liked_movies , 
+        'status' : 'success'
+    })
 
 @app.route("/dislike", methods=["POST"])
 def unliked_movie():
     global all_movies
-    global _d
-    not_liked_movies.append(_d)
-    all_movies=all_movies.iloc[1: , :]
+
+    movie_data=assign_val()
+    not_liked_movies.append(movie_data)
+    all_movies.drop([0], inplace=True)
+    all_movies=all_movies.reset_index(drop=True)
+    
     return jsonify({
         "status": "success"
-    }), 201
+    })
 
-@app.route('/liked' , methods = ['GET'])
-def liked():
-    global liked_movies
-    response = {'data' : liked_movies , 'status' : 'success'}
-    return jsonify(response) , 201
-  
+@app.route("/did_not_watch", methods=["POST"])
+def did_not_watch_view():
+    global all_movies
 
-
-# @app.route("/did_not_watch", methods=["POST"])
-# def did_not_watch_view():
-#     movie = all_movies[0]
-#     did_not_watch.append(movie)
-#     all_movies.pop(0)
-#     return jsonify({
-#         "status": "success"
-#     }), 201
+    movie_data=assign_val()
+    did_not_watch.append(movie_data)
+    all_movies.drop([0], inplace=True)
+    all_movies=all_movies.reset_index(drop=True)
+    
+    return jsonify({
+        "status": "success"
+    })
 
 @app.route("/popular_movies")
 def popular_movies():
-    movie_data = []
-    for movie in output:
+    popular_movie_data = []
+
+    for index, row in output.iterrows():
         _p = {
-            "title": movie[0],
-            "poster_link": movie[1],
-            "release_date": movie[2] or "N/A",
-            "duration": movie[3],
-            "rating": movie[4]
+            "original_title": row['original_title'],
+            "poster_link":row['poster_link'],
+            "release_date":row['release_date'] or "N/A",
+            "duration": row['runtime'],
+            "rating": row['weighted_rating']/2
         }
-        movie_data.append(_p)
+        popular_movie_data.append(_p)
+
     return jsonify({
-        "data": movie_data,
+        "data": popular_movie_data,
         "status": "success"
-    }), 200
+    })
 
 @app.route("/recommended_movies")
 def recommended_movies():
     global liked_movies
-    all_recommended = []
-    print(liked_movies)
+    col_names=['original_title', 'poster_link', 'release_date', 'runtime', 'weighted_rating']
+    all_recommended = pd.DataFrame(columns=col_names)
+    
     for liked_movie in liked_movies:
-        output = get_recommendations(liked_movie[8])
-        for data in output:
-            all_recommended.append(data)
-    import itertools
-    all_recommended.sort()
-    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
-    movie_data = []
-    for recommended in all_recommended:
+        output = get_recommendations(liked_movie["original_title"])
+        all_recommended=all_recommended.append(output)
+
+    all_recommended.drop_duplicates(subset=["original_title"],inplace=True)
+
+    recommended_movie_data=[]
+
+    for index, row in all_recommended.iterrows():
         _p = {
-            "title": recommended[0],
-            "poster_link": recommended[1],
-            "release_date": recommended[2] or "N/A",
-            "duration": recommended[3],
-            "rating": recommended[4]
+            "original_title": row["original_title"],
+            "poster_link":row['poster_link'],
+            "release_date":row['release_date'] or "N/A",
+            "duration": row['runtime'],
+            "rating": row['weighted_rating']/2
         }
-        movie_data.append(_p)
+        recommended_movie_data.append(_p)
+
     return jsonify({
-        "data": movie_data,
+        "data":recommended_movie_data,
         "status": "success"
-    }), 200
+    })
 
 if __name__ == "__main__":
   app.run()
